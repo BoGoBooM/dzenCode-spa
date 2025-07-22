8387bc8002f15288ab00f2c4266e20b128c5fd2b
BoGoBooM <myPikcha@gmail.com>
2025-07-22 10:44:46 +0300

    try to rix 404 error

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Socket, io } from 'socket.io-client';
 
-const SOCKET_URL = 'ws://localhost:3000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [activeSessions, setActiveSessions] = useState(0);
 
   useEffect(() => {
+    if (!SOCKET_URL) return;
+
     const socketIo = io(SOCKET_URL);
 
     socketIo.on('connect', () => {
@@ -23,7 +25,7 @@ export const useSocket = () => {
     return () => {
       socketIo.disconnect();
     };
-  },[]);
+  }, []);
 
   return { socket, activeSessions };
-}
+};
