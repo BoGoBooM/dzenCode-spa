9909ec823b482037ae2a1ba64c494a15572d9a0a
BoGoBooM <myPikcha@gmail.com>
2025-07-22 10:54:01 +0300

    useSocket was updated

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -5,18 +5,18 @@ const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
-  const [activeSessions, setActiveSessions] = useState(0);
+  const [activeSessions, setActiveSessions] = useState<number>(0);
 
   useEffect(() => {
     if (!SOCKET_URL) return;
 
-    const socketIo = io(SOCKET_URL);
+    const socketIo: Socket = io(SOCKET_URL);
 
     socketIo.on('connect', () => {
       console.log('Connected to WebSocket');
     });
 
-    socketIo.on('sessionCount', (count: number) => {
+    socketIo.on('active sessions', (count: number) => {
       setActiveSessions(count);
     });
 
